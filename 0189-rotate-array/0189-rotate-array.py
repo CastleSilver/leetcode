4757f500ca464be942bfdb50f1036652a521eede
Seongeun Heo <95426849+CastleSilver@users.noreply.github.com>
2024-05-02 07:43:33 -0700

    Time: 139 ms (96.22%), Space: 23.3 MB (34.30%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        length = len(nums)
+        copyNums = nums[:]
+        for n in range(0, length):
+            nums[(n+k)%length] = copyNums[n]
