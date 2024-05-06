4d0a6a032cc2ae5e55416fd687c5fc108e3c2979
Seongeun Heo <95426849+CastleSilver@users.noreply.github.com>
2024-05-06 04:56:39 -0700

    Time: 27 ms (50.32%), Space: 11.8 MB (74.73%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.py
+++ 0162-find-peak-element/0162-find-peak-element.py
@@ -4,18 +4,15 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        if len(nums) <= 1:
-            return 0
-        i = 0
-        j = len(nums) - 1
-
-        while i < j:
-            mid = (i+j) // 2
-
-            if nums[mid] > nums[mid+1]:
-                j = mid
+        n = len(nums)
+        low, high = 0, n-1
+        while low <= high:
+            mid = ((high-low)//2) + low
+            # Peak element is present in right side
+            if mid < n-1 and nums[mid] < nums[mid+1]:
+                low = mid+1
+            # Peak element is present in left side
+            elif mid > 0 and nums[mid] < nums[mid-1]:
+                high = mid-1
             else:
-                i = mid + 1
-            
-
-        return i
+                return mid
