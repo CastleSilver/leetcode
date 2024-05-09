1eead2549725eebff6a9474527cd219c22521008
Seongeun Heo <95426849+CastleSilver@users.noreply.github.com>
2024-05-09 23:11:02 +0900

    Time: 94 ms (37.34%), Space: 12.5 MB (79.24%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -4,13 +4,19 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        left, right = 0, 0
+        l = len(nums)
+        i = 0
+        mp = 0
+        if l==1:
+            return 0
         ans = 0
-        while right < len(nums) - 1:
-            maxIdx = 0
-            for i in range(left, right+1):
-                maxIdx = max(maxIdx, i + nums[i])
-            left = right+1
-            right = maxIdx
-            ans+=1
-        return ans
+        memo = [0]
+        while i <= mp:
+            if i+nums[i]>mp and i+nums[i]>memo[-1]:
+                memo.append(i+nums[i])
+            if i == mp:
+                mp = memo[-1]
+                ans += 1
+            if mp>=l-1:
+                return ans
+            i += 1
