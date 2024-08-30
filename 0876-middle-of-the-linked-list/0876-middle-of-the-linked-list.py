e24fc511306e17efd36f7edffa64c947a2e5686d
Seongeun Heo <95426849+CastleSilver@users.noreply.github.com>
2024-08-30 15:45:30 +0900

    Time: 38 ms (33.13%), Space: 16.3 MB (89.64%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
@@ -5,21 +5,8 @@
 #         self.next = next
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        end = 0
-        arr = collections.deque()
-        arr.append(head)
-        while arr:
-            node = arr.popleft()
-            end += 1
-            if node.next:
-                arr.append(node.next)
-        
-        middle = end//2 + 1
-        arr.append(head)
-        while middle > 0:
-            middle -= 1
-            node = arr.popleft()
-            if middle == 0:
-                return node
-            if node.next:
-                arr.append(node.next)
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
