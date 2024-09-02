68067d685ca3add0e187e3a968e374607c4ce9a1
Seongeun Heo <95426849+CastleSilver@users.noreply.github.com>
2024-09-01 19:10:07 -0700

    Time: 54 ms (66.27%), Space: 16.6 MB (64.84%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,13 +1,15 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        if len(ransomNote) > len(magazine): return False
+
         ch_list = defaultdict(int)
 
         for s in magazine:
             ch_list[s] += 1
         
         for ch in ransomNote:
-            ch_list[ch] -= 1
-            if ch_list[ch] < 0:
+            if ch_list[ch] <= 0:
                 return False
+            ch_list[ch] -= 1
         
         return True
